6b1be49358675d308b2c2f8c88f1f2899ccb000c
caseylitton <caseylitton@gmail.com>
2017-06-26 15:04:47 -0700

    Update to version 0.1.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Tox(TestCommand):
 
 setup(
     name="xblock-free-text-response",
-    version="0.1.3",
+    version="0.1.4",
     description="Enables instructors to create questions with free-text responses.",
     license='AGPL-3.0',
     packages=[
