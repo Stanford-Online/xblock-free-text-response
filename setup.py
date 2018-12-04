68d9d9d7d0a232504f5734833c66b47eb3757fc7
Giulio Gratta <giuliog@stanford.edu>
2018-12-04 14:10:00 -0800

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class Tox(TestCommand):
 
 setup(
     name="xblock-free-text-response",
-    version="0.2.0",
+    version="0.3.0",
     description="Enables instructors to create questions with free-text responses.",
     license='AGPL-3.0',
     packages=[
