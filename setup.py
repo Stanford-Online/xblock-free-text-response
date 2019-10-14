b71c69fd1afa5811743a205f9db6401e8dec42d9
stv <stv@stanford.edu>
2019-10-14 16:56:26 -0700

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 from setuptools import setup
 
 
-version = '0.5.0'
+version = '1.0.0'
 description = __doc__.strip().split('\n')[0]
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.rst')) as file_in:
